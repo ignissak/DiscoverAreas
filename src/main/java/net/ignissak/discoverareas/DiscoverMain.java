0614d137245378c15ae258c14f52c410d8976201
Jacob Bordas <methalicity@gmail.com>
2019-11-08 20:24:45 +0100

    Changed resource link

--- src/main/java/net/ignissak/discoverareas/DiscoverMain.java
+++ src/main/java/net/ignissak/discoverareas/DiscoverMain.java
@@ -90,7 +90,7 @@ public final class DiscoverMain extends JavaPlugin {
                 if (getConfiguration().getBoolean("general.update-notify")) {
                     getSmartLogger().info("---------------------------------");
                     getSmartLogger().info("There is a new update available - v" + spigotVersion + ".");
-                    getSmartLogger().info("https://www.spigotmc.org/resources/discoverareas-1-13." + this.resourceID + "/");
+                    getSmartLogger().info("https://www.spigotmc.org/resources/discoverareas-1-12." + this.resourceID + "/");
                     getSmartLogger().info("---------------------------------");
                 }
                 this.updateAvailable = true;
