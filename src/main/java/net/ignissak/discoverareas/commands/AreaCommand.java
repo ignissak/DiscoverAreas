9ee2bcdd7716947778ff296b16063aff6f60db48
Jacob Bordas <methalicity@gmail.com>
2019-11-08 20:05:26 +0100

    Fix /area setxp

--- src/main/java/net/ignissak/discoverareas/commands/AreaCommand.java
+++ src/main/java/net/ignissak/discoverareas/commands/AreaCommand.java
@@ -130,7 +130,7 @@ public class AreaCommand implements CommandExecutor, TabCompleter, Listener {
                     }
                 case "setexp":
                 case "setxp":
-                    // /area setxp <xp> <name>
+                    // /area setxp <name> <xp>
                     if (args.length >= 3) {
                         try {
                             StringBuilder sb = new StringBuilder();
@@ -142,7 +142,7 @@ public class AreaCommand implements CommandExecutor, TabCompleter, Listener {
                             }
 
                             String name = sb.toString();
-                            Integer xp = Integer.parseInt(args[1]);
+                            Integer xp = Integer.parseInt(args[0 + name.split(" ").length]);
 
                             if (!DiscoverMain.getInstance().existsArea(name)) {
                                 ChatInfo.error(player, "Area with name '" + name + "' does not exist.");
